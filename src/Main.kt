4b94672f7b3509650b0435b2a1f5745be4643025
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-12-30 01:06:52 -0500

    Kotlin Version

---
+++ src/Main.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.util.*
+
+object Main {
+    @Throws(IOException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val scan = Scanner(System.`in`)
+        println("Enter String to look for: ")
+        val look = scan.nextLine()
+        println("Enter Folder Path Name: ")
+        val path = scan.nextLine()
+        scan.close()
+        val files = findFiles(path)
+        var reader: BufferedReader
+        for (file in files) {
+            var content = ""
+            reader = BufferedReader(FileReader(file))
+            var line = reader.readLine()
+            while (line != null) {
+                if (!line.contains(look)) content = content + line + System.lineSeparator()
+                line = reader.readLine()
+            }
+            val writer = FileWriter(file)
+            writer.write(content)
+            reader.close()
+            writer.close()
+        }
+        println("Finished!")
+    }
+
+    private fun findFiles(path: String): List<File> {
+        val folder = File(path)
+        return Arrays.stream(Objects.requireNonNull(folder.listFiles())).toList()
+    }
+}
